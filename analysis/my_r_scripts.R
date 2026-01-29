36ef43df9799be99366c31ceaaf7c3b434eaf38e
Angelina Uno-Antonison <ange.unoantonison@gmail.com>
2026-01-29 15:24:27 -0600

    adding first R script

---
+++ analysis/my_r_scripts.R
@@ -0,0 +1,41 @@
+#this is a comment
+101 + 100
+3 + 5 * 10
+
+# help me remmeber order of operation
+3 + (5 * 10)
+
+# do i need a multiplcation symbol? yes
+(3 + 5) * 10
+
+# exponents
+3 ^ 2
+
+# are these equal?
+10 == 10
+9 == 10
+
+# computers aren't great with decimals
+
+10.01 == 10.01
+10.01 == 10.00000000000001
+10.0 == 10.000000000000000000000000000000000000000001
+
+
+# R has functions
+sin(1)
+getwd()
+
+x <- 1/40
+
+# R has vectors
+1:5
+x <- 3:10
+
+# vectors can be used in computations
+
+2 ^ (x)
+
+install.packages('tidyverse')
+library(tidyverse)
+ 
